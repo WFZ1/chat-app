0dbd5595507d5bae5513b0350f67ea5e4e425013
WFZ1 <pavelskumenko@gmail.com>
2021-12-14 10:49:07 +0200

    feat: add authentication

--- chat-backend/router/auth.js
+++ chat-backend/router/auth.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
+const { login, register } = require('../controllers/authController');
+const { validate } = require('../validators');
+const { rules: registrationRules } = require('../validators/auth/register');
+const { rules: loginRules } = require('../validators/auth/login');
 
-router.post('/login', (req, res) => {
-  return res.send(['Login', req.body]);
-});
+router.post('/login', [loginRules, validate], login);
 
-router.post('/register', (req, res) => {
-  return res.send('Register screen works now');
-});
+router.post('/register', [registrationRules, validate], register);
 
 module.exports = router;
